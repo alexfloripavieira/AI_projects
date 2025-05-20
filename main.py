4178ce433c172ea822ac7ec13e00a2de6f78e1a2
alexfloripavieira <alexsandervieira1974@gmail.com>
2025-05-19 22:01:32 -0300

    adiciona arquivo .env com chave da API OpenAI e atualiza .gitignore para incluir venv

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPEN_AI_API_KEY"))
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Me fale mais sobre como estudar IA"}],
+    stream=True,
+)
+
+for chunk in response:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
