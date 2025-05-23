73e6b6020eb62c7fb8244ece1287a13b270d907e
alexfloripavieira <alexsandervieira1974@gmail.com>
2025-05-22 21:31:56 -0300

    adiciona novos arquivos Python para manipulação de documentos PDF e CSV, implementa integração com a API OpenAI e atualiza o arquivo requirements.txt

---
+++ 14.py
@@ -0,0 +1,26 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+
+pdf_path = "laptop_manual.pdf"
+loader = PyPDFLoader(pdf_path)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+chunks = text_splitter.split_documents(
+    documents=docs,
+)
+
+persist_directory = "db"
+
+embedding = OpenAIEmbeddings()
+vector_store = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding,
+    persist_directory=persist_directory,
+    collection_name="laptop_manual",
+)
